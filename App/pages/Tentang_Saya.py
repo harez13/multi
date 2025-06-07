8e3881b4c8c73de17643a1d9bba0369b4c3ec317
harez13 <93501390+harez13@users.noreply.github.com>
2025-06-07 18:56:05 +0700

    Update Tentang_Saya.py

--- App/pages/Tentang_Saya.py
+++ App/pages/Tentang_Saya.py
@@ -8,7 +8,7 @@ st.set_page_config(page_title="YouTube Channel Dashboard", layout="wide")
 # Helper functions
 @st.cache_data
 def load_data():
-    data = pd.read_excel("Data\Data Indeks Standar Pencemar Udara (ISPU) di Provinsi DKI Jakarta ).xls.xlsx")
+    data = pd.read_excel("Data/Data Indeks Standar Pencemar Udara (ISPU) di Provinsi DKI Jakarta ).xls.xlsx")
     data['tanggal'] = pd.to_datetime(data['tanggal'])
     return data
 
